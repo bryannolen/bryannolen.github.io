27fa9c26a8e012b5801bdcb3f38d8697d7712447
Bryan Nolen <bryan@arc.net.au>
2018-06-22 20:16:29 +1000

    Update scripts.js

--- die/js/scripts.js
+++ die/js/scripts.js
@@ -18,11 +18,15 @@ $(document).ready(function() {
     $("#roll_4x").click(function(){die_multi=4});
     $("#roll_5x").click(function(){die_multi=5});
     $("#roll_6x").click(function(){die_multi=6});
+    $("#roll_7x").click(function(){die_multi=7});
+    $("#roll_8x").click(function(){die_multi=8});
     $("#roll_d4").click(function(){rollDie(4)});
     $("#roll_d6").click(function(){rollDie(6)});
     $("#roll_d8").click(function(){rollDie(8)});
     $("#roll_d10").click(function(){rollDie(10)});
     $("#roll_d20").click(function(){rollDie(20)});
+    $("#roll_d50").click(function(){rollDie(50)});
+    $("#roll_d100").click(function(){rollDie(100)});
     $("#last_roll_big").html("Click a Die to Roll!");
     $("#last_roll_list").html("");
 });
