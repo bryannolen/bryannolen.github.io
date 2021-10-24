abe639e428c69de6c57f1f95a29881ad27331fc4
Bryan Nolen <bryan@arc.net.au>
2021-10-24 13:16:52 +1100

    Update status.js

--- D2PCStatus/status.js
+++ D2PCStatus/status.js
@@ -20,7 +20,7 @@ let getJSON = (url, callback) => {
 
 let getStatus = () => {
 
-    getJSON('https://game-status-api.ubisoft.com/v1/instances?appIds=6c6b8cd7-d901-4cd5-8279-07ba92088f06', (err, data) => {
+    getJSON('https://game-status-api.ubisoft.com/v1/instances?appIds=6c6b8cd7-d901-4cd5-8279-07ba92088f06,d2408c33-775c-416b-b5d4-768bd23ccbad', (err, data) => {
         var date = new Date();
         var infoDiv = document.getElementById("info");
         if (err != null) {
@@ -29,11 +29,15 @@ let getStatus = () => {
             let text = `${data[0].Name}<br/>
     Status: ${data[0].Status}<br/>
     Maint: ${data[0].Maintenance}<br/>
+    Last Update: ${date}<br/>
+    ${data[1].Name}<br/>
+    Status: ${data[1].Status}<br/>
+    Maint: ${data[2].Maintenance}<br/>
     Last Update: ${date}<br/>`
 
-            if (data[0].Status == "Degraded") {
+            if (data[0].Status == "Degraded" || data[1].Status == "Degraded") {
                 infoDiv.style.backgroundColor="red"
-            } else if (data[0].Status == "Online") {
+            } else if (data[0].Status == "Online" || data[1].Status == "Online") {
                 infoDiv.style.backgroundColor="green"
             } else {
                 infoDiv.style.backgroundColor="gray"
