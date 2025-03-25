81bd0464e94df53c952e131a17f72b367b1c2be7
BryanNolen <bryan@arc.net.au>
2025-03-25 16:04:23 +1100

    Add XIV status

--- D2PCStatus/status.js
+++ D2PCStatus/status.js
@@ -18,11 +18,11 @@ let getJSON = (url, callback) => {
     xhr.send();
 };
 
-let getStatus = () => {
+let getD2Status = () => {
 
     getJSON('https://game-status-api.ubisoft.com/v1/instances?appIds=6c6b8cd7-d901-4cd5-8279-07ba92088f06,d2408c33-775c-416b-b5d4-768bd23ccbad', (err, data) => {
         var date = new Date();
-        var infoDiv = document.getElementById("info");
+        var infoDiv = document.getElementById("D2Info");
         if (err != null) {
             console.error(err);
         } else {
@@ -53,6 +53,33 @@ let getStatus = () => {
 
 };
 
-getStatus();
+let getXIVStatus = () => {
 
-setInterval(function(){ getStatus(); },60000);
+    getJSON('https://frontier.ffxiv.com/worldStatus/gate_status.json', (err, data) => {
+        var date = new Date();
+        var infoDiv = document.getElementById("XIVInfo");
+        if (err != null) {
+            console.error(err);
+        } else {
+            let text = `Gate Status: ${data.status}<br/>
+    <br/>
+    Last Update: ${date}<br/>`
+
+            if (data[0].status == "0")  {
+            } else if (data[0].status == "1") {
+                infoDiv.style.backgroundColor="green"
+            } else {
+                infoDiv.style.backgroundColor="gray"
+            }
+            infoDiv.style.color = "white";
+            infoDiv.innerHTML = text;
+            console.log(text);
+        }
+    });
+
+};
+
+getD2Status();
+getXIVStatus();
+
+setInterval(function(){ getD2Status(); getXIVStatus(); },60000);
