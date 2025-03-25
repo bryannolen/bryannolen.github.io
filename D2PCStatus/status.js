c4562f78d1fac0cc60312babe1e3a635c5fef969
BryanNolen <bryan@arc.net.au>
2025-03-25 16:08:30 +1100

    Revert "Add XIV status"
    This reverts commit 81bd0464e94df53c952e131a17f72b367b1c2be7.

--- D2PCStatus/status.js
+++ D2PCStatus/status.js
@@ -18,11 +18,11 @@ let getJSON = (url, callback) => {
     xhr.send();
 };
 
-let getD2Status = () => {
+let getStatus = () => {
 
     getJSON('https://game-status-api.ubisoft.com/v1/instances?appIds=6c6b8cd7-d901-4cd5-8279-07ba92088f06,d2408c33-775c-416b-b5d4-768bd23ccbad', (err, data) => {
         var date = new Date();
-        var infoDiv = document.getElementById("D2Info");
+        var infoDiv = document.getElementById("info");
         if (err != null) {
             console.error(err);
         } else {
@@ -53,33 +53,6 @@ let getD2Status = () => {
 
 };
 
-let getXIVStatus = () => {
+getStatus();
 
-    getJSON('https://frontier.ffxiv.com/worldStatus/gate_status.json', (err, data) => {
-        var date = new Date();
-        var infoDiv = document.getElementById("XIVInfo");
-        if (err != null) {
-            console.error(err);
-        } else {
-            let text = `Gate Status: ${data.status}<br/>
-    <br/>
-    Last Update: ${date}<br/>`
-
-            if (data[0].status == "0")  {
-            } else if (data[0].status == "1") {
-                infoDiv.style.backgroundColor="green"
-            } else {
-                infoDiv.style.backgroundColor="gray"
-            }
-            infoDiv.style.color = "white";
-            infoDiv.innerHTML = text;
-            console.log(text);
-        }
-    });
-
-};
-
-getD2Status();
-getXIVStatus();
-
-setInterval(function(){ getD2Status(); getXIVStatus(); },60000);
+setInterval(function(){ getStatus(); },60000);
